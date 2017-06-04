f9e3022a817d2824bc7dc9a8fccb3106ee6678df
rene_mbp <harryloveslearning@googlemail.com>
2017-06-04 11:39:32 +0200

    some cleanup

--- src/app.js
+++ src/app.js
@@ -3,32 +3,14 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
 
 var routeHome = require('./route/home');
 var routeApi = require('./route/api');
 
 var app = express();
 
-/*
-const AUDIENCE = 'https://epl-projectservice.azurewebsites.net/';
-
-const authCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: "https://relang.eu.auth0.com/.well-known/jwks.json"
-    }),
-    audience: AUDIENCE,
-    issuer: "https://relang.eu.auth0.com/",
-    algorithms: ['RS256']
-});
-*/
-
-const AUTH0_SECRET = "HUebW9uSkHZFR1tgGzDAkUSwkSSHa1PVaOTDAkE9vwlDdbNzMS6enwU3RdwCB-8F";
-const AUTH0_AUDIENCE =     "bJGXNSwOrFznt6ZYey6xDOsSb2IOGw6K";
-    // "uQ5ASdbVcuUaRjTSaRwKKMK40gjl44fp",
+const AUTH0_SECRET = process.env.AUTH0_SECRET;
+const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE;
 
 var authCheck = jwt({
 	secret: new Buffer(AUTH0_SECRET),
@@ -38,16 +20,10 @@ var authCheck = jwt({
 app.use(cors());
 // app.use(jwtCheck);
 
-
 app.use(logger('dev'));
 
 app.use("/", routeHome);
-
 app.use("/api/v1/", authCheck, routeApi);
-
-
-
-
 
 var port = process.env.PORT || 3000;
 
